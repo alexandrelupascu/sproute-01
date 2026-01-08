0b16c24d6d02687c340eec8e2202a3a3d4b1624d
Alexandre Lupascu <92180615+alexandrelupascu@users.noreply.github.com>
2026-01-07 22:05:48 -0500

    Started execution logic

--- Assets/Code/Scripts/CombatScripts/CombatHandler.cs
+++ Assets/Code/Scripts/CombatScripts/CombatHandler.cs
@@ -1,16 +1,59 @@
+using System;
 using UnityEngine;
 
 public class CombatHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] WeaponData _currentWeapon;
+    [SerializeField]
+    EffectExecutor _effectExecutor;
+    void Awake()
     {
-        
+        if (_currentWeapon == null)
+        {
+            Debug.LogWarning("CombatHandler: No weapon assigned", this);
+        }
+
+        if (_effectExecutor == null)
+        {
+            _effectExecutor = new EffectExecutor();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void SwitchWeapon(WeaponData newWeapon)
+    {
+        _currentWeapon = newWeapon;
+    }
+
+
+    public void ExecuteAttack(Vector3 origin)
+    {
+        switch (_currentWeapon.DeliveryType)
+        {
+            case WeaponDeliveryType.Melee:
+                ExecuteMeleeAttack(origin, Vector3.zero);
+                break;
+            case WeaponDeliveryType.Ranged:
+                ExecuteRangedAttack(origin, Vector3.zero);
+                break;
+            default:
+                Debug.LogWarning("CombatHandler: Unknown delivery type");
+                break;
+        }
+    }
+
+    private void ExecuteRangedAttack(Vector3 origin, Vector3 target)
+    {
+
+    }
+
+    private void ExecuteMeleeAttack(Vector3 origin, Vector3 target)
+    {
+
     }
 }
